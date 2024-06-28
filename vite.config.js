279178038bec8e133a1df0a16cb16bf34ec0d7b9
史客郎 <13261539113@163.com>
2024-06-28 19:45:24 +0800

    release

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://autumnriver-a.github.io/Subtle-Stirrings-fe/'
+  base: 'Subtle-Stirrings-fe'
 })
