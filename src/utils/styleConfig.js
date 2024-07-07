e9f97c5c3bf7d764caeec25964d80d479229f21b
史客郎 <13261539113@163.com>
2024-07-08 03:00:18 +0800

    update

---
+++ src/utils/styleConfig.js
@@ -0,0 +1,13 @@
+const styleConfig = {
+    "timeline": {},
+    "earth": {
+        "ocean-color": "rgb(95,173,209)",
+        "land-color": "rgb(181,228,207)",
+        "country-color": "#fff",
+        "country-border": 0.6,
+        "background": "@/assets/background4.jpg",
+        "globe-size": 800
+    }
+}
+
+export default styleConfig;
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,16 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   // base: '/home',
   resolve: {
+    extensions: ['.js', '.vue', '.json', 'jpg'],
     alias: {
       '@': '/src', // 将 @ 符号指向项目根目录下的 src 目录
+      // '@': resolve(__dirname, 'src')
     }
   },
   server: {
