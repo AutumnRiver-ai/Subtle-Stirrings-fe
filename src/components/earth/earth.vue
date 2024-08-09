098e47f4f2b8f579acf9bac2197f636dabaa0a17
史客郎 <13261539113@163.com>
2024-08-09 16:47:31 +0800

    update

---
+++ src/components/earth/earth.vue
@@ -0,0 +1,216 @@
+<template>
+    <div id="globe-container" class="globe-container">
+        <transition name="globe-fade">
+            <div id="globe" v-show="!cardShow"></div>
+        </transition>
+    </div>
+    <transition name="card-fade" class="card">
+        <a-card hoverable style="max-width: 100%; max-height: 80%; z-index: -1;" v-show="cardShow">
+            <template #cover>
+                <img alt="example" :src="cardInfo.cardImg" />
+            </template>
+            <a-card-meta :title="cardInfo.cardContent">
+                <!-- <template #description>
+                    {{ cardInfo.cardContent }}
+                </template> -->
+            </a-card-meta>
+        </a-card>
+    </transition>
+</template>
+
+<script setup lang='ts'>
+import * as d3 from 'd3';
+import { onMounted, computed, render, h, reactive, ref } from 'vue';
+import { useStore } from 'vuex';
+import { EnvironmentTwoTone } from '@ant-design/icons-vue';
+import { Tooltip } from 'ant-design-vue';
+import styleConfig from '@/utils/styleConfig.js';
+const store = useStore();
+
+interface CardInfo {
+    cardContent: string;
+    cardImg: string;
+}
+
+const cardInfo: CardInfo = reactive({
+    cardContent: "",
+    cardImg: ''
+})
+let cardShow = ref(false)
+// const LANDCOLOR = 'lightblue'
+// const COUNTRYCOLOR = 'lightblue'
+// const OCEANCOLOR = '#bbb'
+
+const OCEANCOLOR = styleConfig['earth']['ocean-color']
+const LANDCOLOR = styleConfig['earth']['land-color']
+const COUNTRYCOLOR = styleConfig['earth']['country-color']
+const COUNTRYBORDER = styleConfig['earth']['country-border']
+const SIZE = styleConfig['earth']['globe-size']
+onMounted(() => {
+    // return
+    // 地图投影
+    const projection = d3.geoOrthographic()
+        .scale(SIZE / 2 - 0.5)
+        .translate([SIZE / 2, SIZE / 2])
+
+    const path = d3.geoPath().projection(projection);
+
+    // 创建 SVG 元素
+    const svg = d3.select("#globe")
+        .append("svg")
+        .attr("width", SIZE)
+        .attr("height", SIZE)
+        .style("background-color", OCEANCOLOR)
+        .style('border-radius', '50%');
+
+    // 粗糙地图 https://raw.githubusercontent.com/epistler999/GeoLocation/master/world.json
+    d3.json("https://raw.githubusercontent.com/epistler999/GeoLocation/master/world.json")
+        .then(data => {
+            svg.append("g")
+                .selectAll("path")
+                .data(data['features'])
+                .enter()
+                .append("path")
+                .attr("d", path)
+                .attr("fill", LANDCOLOR)
+                .attr("stroke", COUNTRYCOLOR)
+                .attr("stroke-width", COUNTRYBORDER);
+            // 添加旋转动画
+            centerPosition(svg, projection, path, {
+                location: [116, 39],
+                preLocation: [116, 39]
+            }, false);
+
+            store.subscribe((mutation, state) => {
+                if (mutation.type === 'updateContent') {
+                    centerPosition(svg, projection, path, state);
+                }
+            });
+        })
+});
+const centerPosition = (svg, projection, path, state, animate = true) => {
+    const longitude = state.location[0];
+    const latitude = state.location[1];
+    const preLongitude = state.preLocation[0]
+    const preLatitude = state.preLocation[1]
+    // const preLongitude = 116
+    // const preLatitude = 40
+    const pointStart = [-preLongitude, 23 - preLatitude]
+    const pointEnd = [-longitude, 23 - latitude]
+
+    cardShow.value = false
+    svg.select('foreignObject').remove();
+    let duration = 2500;
+    if (!animate) {
+        duration = 10;
+    }
+    svg.transition()
+        .duration(duration) // 设置动画持续时间为 2 秒
+        .tween("rotate", () => {
+            const interpolateRotation = d3.interpolate(pointStart, pointEnd);
+            return function (t) {
+                projection.rotate(interpolateRotation(t));
+                svg.selectAll("path").attr("d", path);
+            };
+        })
+        .on("end", () => {
+            createMarker(svg, projection, path, pointEnd);
+            setTimeout(() => {
+                if (animate) {
+                    cardShow.value = true
+                    if (state.content) {
+                        cardInfo.cardImg = state.content.img;
+                        cardInfo.cardContent = state.content.content;
+                    }
+                }
+            }, 1500)
+        });
+
+
+}
+const createMarker = (svg, projection, path, point) => {
+    const longitude = point[0];
+    const latitude = point[1];
+    const [x, y] = projection([-longitude, 23 - latitude]);
+    let [offsetX, offsetY] = projection.translate()
+    offsetX = offsetX - SIZE / 2;
+    offsetY = offsetY - SIZE / 2
+    const iconSize = 40
+    svg.append("foreignObject")
+        .attr("x", x + offsetX - iconSize / 2)
+        .attr("y", y + offsetY - iconSize)
+        .attr("width", iconSize)
+        .attr("height", iconSize)
+        // .html("<div id='marker' style='background: red'></div>");
+        .html("<div id='marker'></div>");
+    svg.selectAll("path").attr("d", path);
+    const container = document.getElementById('marker');
+    const icon = h(EnvironmentTwoTone, { spin: false, rotate: 0, style: { fontSize: '40px', color: 'red' } });
+    // const tooltip = h(Tooltip, { title: '当前位置', id: 'tooltip' }, { default: () => icon });
+    render(icon, container);
+}
+</script>
+
+<style scoped>
+.globe-container {
+    top: 0px;
+    position: absolute;
+    min-width: 100%;
+    max-width: 100%;
+    padding: 0 20% 0 0;
+    height: calc(100vh - 220px);
+    z-index: -1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url('@/assets/background9.jpg');
+    background-size: cover;
+    background-position: center;
+    /* opacity: 0; */
+
+    .globe-fade-enter-active {
+        transition: all 1.2s ease-out;
+    }
+
+    .globe-fade-leave-active {
+        transition: all 1.2s cubic-bezier(1, 0.5, 0.8, 1);
+    }
+
+    .globe-fade-enter-from,
+    .globe-fade-leave-to {
+        transform: translateX(20px);
+        opacity: 0;
+    }
+}
+
+
+.card {
+    position: absolute;
+    /**这是窄屏的 */
+    left: 5%;
+    top: 10%;
+    width: 90%;
+    /**这是宽屏的 */
+    /* left: 40%;
+    top: 15%;
+    width: 30%; */
+
+    padding: 0;
+    box-shadow: 10px 10px 10px #ccc;
+    border-radius: 10px;
+}
+
+.card-fade-enter-from,
+.card-fade-leave-to {
+    transform: translateX(20px);
+    opacity: 0;
+}
+
+.card-fade-enter-active {
+    transition: all 1.2s ease-out;
+}
+
+.card-fade-leave-active {
+    transition: all 1.2s cubic-bezier(1, 0.5, 0.8, 1);
+}
+</style>
---
+++ src/components/timeline/timeline.vue
@@ -0,0 +1,350 @@
+<template>
+    <div id="timeline_container">
+        <div id="toolbox">
+            <HomeTwoTone :style="{ fontSize: '20px' }" @click="toolClick('home')" />
+            <PlayCircleTwoTone :style="{ fontSize: '20px' }" @click="toolClick('play')" v-if="!recording" />
+            <PauseCircleTwoTone :style="{ fontSize: '20px' }" @click="toolClick('stop')" v-if="recording" />
+            <PlusCircleTwoTone :style="{ fontSize: '20px' }" @click="toolClick('magnify')" />
+            <MinusCircleTwoTone :style="{ fontSize: '20px' }" @click="toolClick('reduce')" />
+        </div>
+        <div class="baseline_v" :style="{ left: `${timeline['baseLineOffset'] * 100}%` }"></div>
+        <div class="baseline_h"></div>
+        <div class="dragable_container" :style="{ left: timeline.offset + 'px' }"
+            v-TimelineDrag="{ timeline: timeline, refreshFlag: refreshFlag }">
+            <div class="flagwraper">
+                <div v-for="(flag, index) in flags" :key="index"
+                    v-TimelineFlag="{ flag: flag, timeline: timeline, refreshFlag: refreshFlag, index: index }"
+                    class="timeflag" @click="clickFlag(index)">
+                    <div class="marker" :class="{ 'marker_active': index == timeline.activeFlag }">
+                        <img class="icon" :src=iconURL>
+                        <div class='content'>{{ flag['title'] }}</div>
+                    </div>
+                    <div class="pole">
+                        <div class="poleheader"></div>
+                        <div class="polefooter"></div>
+                    </div>
+                </div>
+
+                <div v-for="(rulermarker, i) in timeline.rulerMarkers" :key="i" class="rulermarker"
+                    :style="{ left: rulermarker.position + 'px' }"
+                    :class="{ 'year_type': rulermarker.timeType == 'year' }">
+                    {{ rulermarker.content }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang='ts'>
+import { ref, reactive, onMounted, onBeforeMount } from 'vue';
+import axios from 'axios';
+import timelineConfig from '@/components/timeline/timeline.config.js';
+import iconURL from '@/assets/icon.jpg';
+import { renderTimeline } from '@/components/timeline/renderTimeline.js';
+import { PlusCircleTwoTone, MinusCircleTwoTone, HomeTwoTone, PlayCircleTwoTone, PauseCircleTwoTone } from '@ant-design/icons-vue';
+import { useStore } from 'vuex';
+import { useRoute, useRouter } from 'vue-router';
+import { recordVideo, stopRecordVideo } from '@/utils/record.js';
+const ANIMATEDURATION = 2700 + 2000 + 550
+const router = useRouter()
+
+const timelineData = timelineConfig;
+const flags = timelineData["flags"];
+const timeline = timelineData["timeline"];
+const refreshFlag = ref(true)
+const store = useStore();
+const vm = new renderTimeline()
+let recording = ref(false);
+
+onMounted(() => {
+    renderFlags();
+})
+
+const renderFlags = () => {
+    vm.render(timelineData);
+    refreshFlag.value = !refreshFlag.value
+}
+const clickFlag = (index) => {
+    goToFlag(index);
+    refreshFlag.value = !refreshFlag.value
+    const flag = flags[index]
+    store.commit('updateContent', {
+        content: {
+            'content': flag['content'],
+            'img': flag['img']
+        },
+        location: flag['location']
+    });
+};
+
+const goToFlag = (index) => {
+    timeline.activeFlag = index;
+    vm.positionFlags(timelineData);
+    timeline.init = true;
+};
+
+const toolClick = async (type) => {
+    const factor = 1.5
+    if (type === 'magnify') {
+        timelineData.timeline.ruler *= factor;
+        timelineData.timeline.rulerNum *= factor
+    } else if (type === 'reduce') {
+        timelineData.timeline.ruler /= factor;
+        timelineData.timeline.rulerNum /= factor
+    } else if (type === 'home') {
+        router.replace({
+            name: 'news'
+        })
+    } else if (type == 'play') {
+        recording.value = true
+        await recordVideo();
+        setTimeout(() => {
+            play(0);
+        }, 2000)
+
+    } else if (type == 'stop') {
+        await stopRecordVideo();
+        recording.value = false
+    }
+    renderFlags();
+};
+
+const orderFlags = () => {
+    let playOrder = []
+    flags.forEach((element, index) => {
+        playOrder.push({
+            order: element['order'],
+            index: index
+        })
+    });
+    playOrder.sort((a, b) => a.order - b.order)
+    return playOrder
+}
+
+const play = (index) => {
+    const playOrder = orderFlags()
+    const flagIndex = playOrder[index]['index']
+    let duration = flags[flagIndex]['duration'] * 1000 + ANIMATEDURATION
+    // let duration = 3000
+    // let duration = 1 * 1000 + ANIMATEDURATION
+    clickFlag(flagIndex);
+    setTimeout(() => {
+        index += 1
+        if (index != flags.length) {
+            play(index)
+        } else {
+            toolClick('stop')
+        }
+
+    }, duration)
+}
+
+</script>
+
+<style scoped>
+#timeline_container {
+
+    position: absolute;
+    bottom: 0px;
+    box-shadow: inset 0 10px 10px 0px rgba(255, 255, 255, 0.6);
+    background: linear-gradient(to right top, rgba(100, 200, 199, 1), rgba(50, 147, 230, 1));
+
+    width: 100%;
+    height: 220px;
+    overflow: hidden;
+
+    #toolbox {
+        position: absolute;
+        top: 42%;
+        transform: translate(0%, -50%);
+        /* 使用translate属性水平垂直居中 */
+        font-size: 12px;
+        height: 76%;
+        width: 30px;
+        background: rgba(253, 254, 240, 0.8);
+        z-index: 100;
+        display: flex;
+        flex-direction: column;
+        /* 设置子元素纵向排列 */
+        justify-content: space-between;
+        padding: 20px 0;
+        box-shadow: 6px 6px 2px #ccc;
+    }
+
+    .baseline_v {
+        position: absolute;
+        height: 190px;
+        width: 1.5px;
+        left: 10%;
+        background: rgb(0, 0, 0, 0.8);
+        z-index: 100;
+    }
+
+    .baseline_h {
+        position: absolute;
+        bottom: 28px;
+        height: 1px;
+        width: 100%;
+        left: 0;
+        background: rgba(225, 225, 225, 0.3);
+        z-index: 100;
+    }
+
+    .dragable_container {
+        cursor: move;
+        position: absolute;
+        height: 215px;
+        width: 200%;
+
+        &::after {
+            position: absolute;
+            right: 280px;
+            top: 50%;
+            transform: translate(0, -50%);
+            content: '';
+            height: 100px;
+            width: 100px;
+            background-image: url('@/assets/qrcode.png');
+            background-position: right;
+            background-repeat: no-repeat;
+            background-size: contain;
+            /* border-radius: 45%; */
+            /* border: 4px solid #fff; */
+        }
+
+        .rulermarker {
+            font-size: 8px;
+            position: absolute;
+            bottom: 0px;
+            z-index: 100;
+            height: 14px;
+            line-height: 12px;
+            color: rgba(255, 255, 255, 1);
+            font-weight: bold;
+            text-align: center;
+            /* left: 50%; */
+            width: 70px;
+            /* background: red; */
+            /* border-right: 1px solid black; */
+            overflow: hidden;
+
+            &::after {
+                content: "";
+                position: absolute;
+                background: rgba(225, 225, 225, 1);
+                width: 1px;
+                height: 6px;
+                bottom: 18px;
+                /* left: 50%; */
+
+            }
+        }
+
+        .year_type {
+            font-size: 10px;
+            width: 70px;
+            height: 14px;
+            color: rgba(225, 225, 225, 1);
+            font-weight: 900;
+
+            &::after {
+                content: "";
+                position: absolute;
+                background: rgba(225, 225, 225, 1);
+                width: 2px;
+                height: 8px;
+                bottom: 16px;
+                left: 50%;
+
+            }
+        }
+
+        .flagwraper {
+            height: 200px;
+
+            .timeflag {
+                cursor: pointer;
+                position: absolute;
+
+                .marker {
+                    position: absolute;
+                    border-radius: 5px;
+                    background: linear-gradient(to bottom, rgba(181, 228, 207, 0.95), rgba(181, 228, 207, 0.85));
+                    border-radius: 0px 10px 10px 0px;
+                    display: flex;
+                    justify-content: space-between;
+                    align-items: center;
+
+                    &::after {
+                        content: "";
+                        position: absolute;
+                        top: 0;
+                        left: 0;
+                        width: 99.5%;
+                        height: 100%;
+                        border-radius: 0px 10px 10px 0px;
+                    }
+
+                    .icon {
+                        margin: 4px;
+                        width: 35px;
+                        height: auto;
+                    }
+
+                    .content {
+                        color: black;
+                        padding: 0 4px;
+                        text-align: left;
+                        width: 100%;
+                        font-size: 14px;
+                        font-weight: bold;
+                        line-height: 40px;
+                    }
+                }
+
+                .marker_active {
+                    background: rgba(167, 247, 139, 1);
+                    /* z-index: 999; */
+                }
+
+                .pole {
+                    width: 1px;
+                    position: absolute;
+                    height: 190px;
+                    top: 0;
+                    background: rgba(0, 0, 0, 0.1);
+                    z-index: 101;
+
+                    .polefooter {
+                        position: relative;
+                        top: 190px;
+                        left: -1.5px;
+                        width: 4px;
+                        height: 4px;
+                        border-radius: 2px;
+                        background: rgb(0, 0, 0);
+                    }
+
+                    .poleheader {
+                        position: absolute;
+                        top: 0px;
+                        width: 0px;
+                        left: -40px;
+                        height: 6px;
+                        background: rgba(0, 0, 0, 0.6);
+                    }
+                }
+            }
+        }
+    }
+}
+
+#testbtn {
+    background: rgb(31, 180, 250);
+    color: #fff;
+    border-radius: 5px;
+    cursor: pointer;
+    width: 130px;
+    margin: 60px auto;
+}
+</style>
